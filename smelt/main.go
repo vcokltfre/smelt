14fe4c497bf5e42953b5b1a0f5246dbcfbff3d8d
vcokltfre <vcokltfre@gmail.com>
2024-01-02 09:27:22 +0000

    feat(smelt): add temporary basic commands

--- smelt/main.go
+++ smelt/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/vcokltfre/smelt/libsmelt/repository"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "init":
+		repo, err := repository.Init("test", ".")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Initialized repository %s at %s\n", repo.Name, repo.Root)
+	case "load":
+		repo, err := repository.Load(".")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Loaded repository %s at %s\n", repo.Name, repo.Root)
+	}
+}
